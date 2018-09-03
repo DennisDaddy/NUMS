0c889557362961e550a6706a8fbc250acdb8b7eb
DennisDaddy <softteez95@gmail.com>
2018-09-03 05:06:21 +0300

    [Chore] Fix question error and update dependencies

--- app/api.py
+++ app/api.py
@@ -27,15 +27,15 @@ class QuestionList(Resource):
         except:
             return jsonify({'message': 'Cannot retrieve entries'})
         return jsonify(my_list)
-    
+
     def post(self):
         """This is a method for creating a question using POST request"""
         title = request.get_json()['title']
         content = request.get_json()['content']
 
-        if len(title) ==0:
+        if len(title)==0:
             return jsonify({'message': 'Fill in the title'})
-        if len(content) ==0:
+        if len(content)==0:
             return jsonify({'message': 'Fill in the content'})
         cur.execute("INSERT INTO questions (title, content) VALUES('"+title+"','"+content+"');")
         conn.commit()
@@ -66,7 +66,7 @@ class Question(Resource):
         else:
             return jsonify({'message': 'Not complete no entry!'})
         conn.commit()
-        return jsonify({'message': 'Entry successfuly Updated'})
+        return jsonify({'message': 'Question successfuly Updated'})
     
     def delete(self, id):
         """This is a method for deleting a question using DELETE request"""
@@ -74,7 +74,7 @@ class Question(Resource):
             cur.execute("DELETE FROM questions WHERE ID = %s", (id,))
             conn.commit()
         except:
-            return jsonify({'message': 'Cant retrieve question!'})
+            return jsonify({'message': 'Cant retrieve the question!'})
         finally: 
             conn.close()
         return jsonify({'message': 'Question successfully deleted!'})
@@ -83,7 +83,7 @@ class Question(Resource):
 class  AnswerList(Resource):
     """This is a class for answers without IDs"""
     def get(self):
-        """This is a method for retrieving answers for a question """
+        """This is a method for retrieving answers for a question using GET request """
         cur.execute("SELECT * FROM answers")
         answers = cur.fetchall()
         if answers is None:
@@ -94,10 +94,10 @@ class  AnswerList(Resource):
 
 
     def post(self):
-        """This is a method for creating an answer for a question"""
+        """This is a method for creating an answer for a question using POST request"""
         body = request.get_json()['body']
 
-        if len(body) ==0:
+        if len(body)==0:
             return jsonify({'message': 'Fill in the body'})
         cur.execute("INSERT INTO answers (body) VALUES('"+body+"');")
         conn.commit()
@@ -106,19 +106,19 @@ class  AnswerList(Resource):
 class Answer(Resource):
     """This is a class for answers with IDs"""
     def get(self,id):
-        """This is a method for getting an answer for a given  question"""
+        """This is a method for getting an answer for a given  question using GET request"""
         cur.execute("SELECT * FROM answers WHERE ID= %s", (id,))
         result = cur.fetchone()
         if result is None:
             return jsonify({'message': 'Answer not found!'})
         return jsonify(result)
 
-    
+
     def put(self, id):
-        """This is a method for modifying an answer for a question"""
+        """This is a method for modifying an answer for a question using PUT request"""
         cur.execute("SELECT * FROM answers WHERE ID= %s", (id,))
         answer = cur.fetchone()
-        
+
         body = request.get_json()['body']     
 
         if answer is not None:
@@ -129,7 +129,7 @@ class Answer(Resource):
         return jsonify({'message': 'Answer successfuly Updated'})
     
     def delete(self, id):
-        """This is a method for deleting an answer for a question"""
+        """This is a method for deleting an answer for a question using DELETE request"""
         try:
             cur.execute("DELETE FROM answers WHERE ID = %s", (id,))
             conn.commit()
@@ -141,7 +141,9 @@ class Answer(Resource):
 
 
 class UserRegistration(Resource):
+    """This a class for registering a user"""
     def post(self):
+        """This a method for registering a user using POST request """
         username = request.get_json()['username']
         email = request.get_json()['email']
         password = request.get_json()['password']
@@ -160,6 +162,7 @@ class UserRegistration(Resource):
 class UserLogin(Resource):
     """This is a class for logging in a user"""
     def post(self):
+        """This is a method for logging in a user"""
         username = request.get_json()['username']
         password = request.get_json()['password']
 
@@ -168,8 +171,7 @@ class UserLogin(Resource):
         rows = cur.fetchone()
         if rows is None:
             return jsonify({'message': 'Not successful you can try again'})
-        else:
-            return jsonify({'message': 'You are successfully logged in!'})
+        return jsonify({'message': 'You are successfully logged in!'})
     conn.commit()
 
 class UserInfo(Resource):
