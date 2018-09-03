3d11e66044ce281b2277348b5d34ce428d209fca
DennisDaddy <softteez95@gmail.com>
2018-09-03 05:37:01 +0300

    [Chore] Test root endpoint

--- tests/test_questions.py
+++ tests/test_questions.py
@@ -7,15 +7,13 @@ from app import *
 sys.path.insert(0, os.path.abspath(".."))
 
 class QuestionTestCase(unittest.TestCase):
-    """This test case represents test for a user and diary entry testcase"""
-    def setUp(self):
-        self.app = create_app(config_name="testing")
-        self.client = self.app.test_client
-
-    def test_index(self):
-        result = self.client().get('/')
-        self.assertEqual(result.status_code,200, {'message': 'Welcome to Stackoverflow-lite'})
-
+	"""This test case represents test for question testcase"""
+	def test_home(self):
+		"""This method tests root endpoint"""
+		tester = app.test_client(self)
+		response = tester.get('/', content_type='application/json')
+		self.assertEqual(response.status_code, 200)
+		self.assertIn(b'Welcome to Stackoverflow-lite', response.data)
 
 
 if __name__ == '__main__':
