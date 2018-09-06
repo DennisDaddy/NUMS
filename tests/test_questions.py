b5779c3f229f51ae21536d753bca152ef8ae4aa9
DennisDaddy <softteez95@gmail.com>
2018-09-06 06:47:20 +0300

    [Chore #159994413] Test endpoint for creating a question

--- tests/test_questions.py
+++ tests/test_questions.py
@@ -8,12 +8,26 @@ sys.path.insert(0, os.path.abspath(".."))
 
 class QuestionTestCase(unittest.TestCase):
 	"""This test case represents test for question testcase"""
+
+
+	def setUp(self):
+		"""Define test variables and initialize the app."""
+		self.app = create_app(config_name="testing")
+		self.client = self.app.test_client
+
+
 	def test_home(self):
 		"""This method tests root endpoint"""
 		tester = app.test_client(self)
 		response = tester.get('/', content_type='application/json')
 		self.assertEqual(response.status_code, 200)
 		self.assertIn(b'Welcome to Stackoverflow-lite', response.data)
+
+	def test_create_question(self):
+		tester = app.test_client(self)
+		response = tester.get('/api/v1/questions', content_type='application/json')
+		self.assertEqual(response.status_code, 200)
+		self.assertIn(b'Question successfully created!', response.data)
 
 
 if __name__ == '__main__':
