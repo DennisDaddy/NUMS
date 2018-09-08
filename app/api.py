111673d37d6da275d3b931a7dcac6788c7a9beb0
DennisDaddy <softteez95@gmail.com>
2018-09-08 22:46:01 +0300

    [Chore] follow pep8 standards

--- app/api.py
+++ app/api.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request, make_response
-from flask_restful import Resource, Api
 import sys
+from flask_restful import Resource, Api
+
 from flask_cors import CORS
 from app.models import *
 
