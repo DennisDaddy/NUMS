01a693f64404683ae2af8c0ac5a41d24ee6f6d6f
DennisDaddy <softteez95@gmail.com>
2018-09-20 09:34:23 +0300

    [Bug] Fix jwt and cors errors

---
+++ test_questions.py
@@ -0,0 +1,51 @@
+import unittest
+import json
+import os
+import sys
+from app import *
+
+sys.path.insert(0, os.path.abspath(".."))
+
+class QuestionTestCase(unittest.TestCase):
+	"""This test case represents test for question testcase"""
+
+
+	def setUp(self):
+		"""Define test variables and initialize the app."""
+		self.app = create_app(config_name="testing")
+		self.client = self.app.test_client
+
+
+	def test_home(self):
+		"""This method tests root endpoint"""
+		tester = app.test_client(self)
+		response = tester.get('/api/v1', content_type='application/json')
+		self.assertEqual(response.status_code, 200)
+		self.assertIn(b'Welcome to Stackoverflow-lite', response.data)
+
+	def test_create_question(self):
+		"""This method tests endpoint for creating a question"""
+		response = self.client().post('/api/v1/questions', content_type='application/json')
+		self.assertEqual(response.status_code, 400)
+	
+	
+	def test_retrieve_all_questions(self):
+		"""Test retrieving questions"""
+		response = self.client().get('/api/v1/questions')
+		self.assertEqual(response.status_code, 200)
+	
+	def test_retrieve_single_question(self):
+		pass
+
+	def test_modify_question(self):
+		"""This method tests endpoint for updating a question"""
+		response = self.client().get('api/v1/questions/1')
+		self.assertEqual(response.status_code, 200)
+	
+	def test_delete_question(self):
+		"""This method tests endpoint for delete a question"""
+		response = self.client().get('api/v1/questions/1')
+		self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ test_user.py
@@ -0,0 +1,49 @@
+import unittest
+import json
+import os, sys
+sys.path.insert(0, os.path.abspath(".."))
+from app import *
+
+class UserTeastCase(unittest.TestCase):
+    """This testcase represents the user model testcase"""
+
+
+    def setUp(self):
+        """Initialize test variables"""
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client()
+        self.user = {
+            'username': "dennis",
+            'email': "dennis@gmail.com",
+            'password': "foobar",
+            'password_confirmation': "foobar"
+        }
+
+    def test_home(self):
+        """This is a method for testing root endpoint"""
+        response = self.client.get('/api/v1')
+        self.assertEqual(response.status_code, 200)    
+
+    def test_register_user(self):
+        """This is a method for testing user registration"""
+        response = self.client.post('/api/v1/auth/register', data=json.dumps(
+            dict(username="dennis", email="dennis@gmail.com", password="foobar",
+            password_confirmation="foobar")),
+            content_type="application/json")
+        self.assertEqual(response.status_code, 200)    
+
+    def test_user_login(self):
+        """This is a method for testing user login"""
+        response = self.client.post('/api/v1/auth/login', data=json.dumps(
+            dict(username="dennis", email="dennis@gmail.com", password="foobar",
+            password_confirmation="foobar")),
+            content_type="application/json")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_user_account(self):
+        """This is a method for testing user account information"""
+        response = self.client.get('/api/v1/account/1')
+        self.assertEqual(response.status_code, 200)
+
+if __name__=='__main__':
+    unittest.main()
