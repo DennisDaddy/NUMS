a613c42dcec1887ceb396d3df1a5a371affbdaa1
DennisDaddy <softteez95@gmail.com>
2018-09-20 13:40:06 +0300

    [Chore] Update tests

--- tests/test_answers.py
+++ tests/test_answers.py
@@ -23,26 +23,26 @@ class AnswersTestCase(unittest.TestCase):
 		self.assertEqual(response.status_code, 200)
 		self.assertIn(b'Welcome to Stackoverflow-lite', response.data)
 
-	# def test_create_answer(self):
-	# 	"""This method tests endpoint for creating an answer"""
-	# 	response = self.client().post('/api/v1/answers', content_type='application/json')
-	# 	self.assertEqual(response.status_code, 400)
+	def test_create_answer(self):
+		"""This method tests endpoint for creating an answer"""
+		response = self.client().post('/api/v1/answers', content_type='application/json')
+		self.assertEqual(response.status_code, 400)
 	
 	
-	# def test_retrieve_all_answers(self):
-	# 	"""Test retrieving answers"""
-	# 	response = self.client().get('/api/v1/answers')
-	# 	self.assertEqual(response.status_code, 200)
+	def test_retrieve_all_answers(self):
+		"""Test retrieving answers"""
+		response = self.client().get('/api/v1/answers')
+		self.assertEqual(response.status_code, 200)
 
-	# def test_modify_answer(self):
-	# 	"""This method tests endpoint for updating an answer"""
-	# 	response = self.client().get('api/v1/questions/1')
-	# 	self.assertEqual(response.status_code, 200)
+	def test_modify_answer(self):
+		"""This method tests endpoint for updating an answer"""
+		response = self.client().get('api/v1/questions/1')
+		self.assertEqual(response.status_code, 200)
 
-	# def test_delete_answer(self):
-	# 	"""This method tests endpoint for deleting an answer"""
-	# 	response = self.client().get('api/v1/answers/1')
-	# 	self.assertEqual(response.status_code, 200)
+	def test_delete_answer(self):
+		"""This method tests endpoint for deleting an answer"""
+		response = self.client().get('api/v1/answers/1')
+		self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
